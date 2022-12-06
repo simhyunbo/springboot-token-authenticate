7c8ca591baa77dae955bd352e5dd5e3563829863
simhyunbo <hbo9347@naver.com>
2022-12-06 23:18:06 +0900

    first commit

---
+++ src/main/java/com/example/tokenauthenticatereview/TokenAuthenticateReviewApplication.java
@@ -0,0 +1,13 @@
+package com.example.tokenauthenticatereview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TokenAuthenticateReviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TokenAuthenticateReviewApplication.class, args);
+	}
+
+}
